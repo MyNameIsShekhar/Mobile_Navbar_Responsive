3d2ebfc034bab16c5f982a71d34a12107c0e94e5
Shekhar <151825448+MyNameIsShekhar@users.noreply.github.com>
2024-03-16 08:58:11 +0530

    Update SearchBar.jsx

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,46 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
-function SearchPage() {
-  const [searchResults, setSearchResults] = useState([]);
-  const location = useLocation();
+function SearchBar() {
+  const [keyword, setKeyword] = useState('');
+  const history = useHistory();
 
-  useEffect(() => {
-    const searchParams = new URLSearchParams(location.search);
-    const searchTerm = searchParams.get('s');
-    if (searchTerm) {
-      // Fetch search results using the searchTerm
-      fetchSearchResults(searchTerm);
+  const handleSearch = () => {
+    let formattedKeyword = keyword.trim().replace(/\s+/g, '-'); // Replace spaces with dashes
+    if (formattedKeyword === '') {
+      console.log('Please enter a keyword to search.');
+      return;
     }
-  }, [location.search]);
-
-  const fetchSearchResults = async (searchTerm) => {
-    try {
-      const response = await fetch(`https://hunt-grab-api.vercel.app/search?keyword=${searchTerm}`);
-      const data = await response.json();
-      setSearchResults(data);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
+    history.push(`/search?s=${formattedKeyword}`);
   };
 
   return (
-    <div className="container mx-auto mt-8">
-      <h1 className="text-3xl font-semibold mb-4">Search Results</h1>
-      <ul>
-        {searchResults.map((result, index) => (
-          <li key={index} className="mb-4">
-            <img src={result.img_src} alt={result.title} className="w-20 h-20 mr-4 rounded-md" />
-            <div>
-              <h3 className="text-lg font-semibold">{result.title}</h3>
-              <p>{result.released}</p>
-              <p><a href={`https://example.com/${result.href_url}`} target="_blank" rel="noopener noreferrer">Watch Now</a></p>
-            </div>
-          </li>
-        ))}
-      </ul>
+    <div className="flex items-center justify-center h-screen">
+      <input
+        type="text"
+        placeholder="Enter keyword..."
+        className="border border-gray-300 rounded-md py-2 px-4 mr-2 focus:outline-none focus:border-blue-400"
+        value={keyword}
+        onChange={(e) => setKeyword(e.target.value)}
+      />
+      <button
+        className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-300"
+        onClick={handleSearch}
+      >
+        Search
+      </button>
     </div>
   );
 }
 
-export default SearchPage;
+export default SearchBar;
